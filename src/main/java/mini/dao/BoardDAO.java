63f3bd1c58e99fad33463c39bb1fb1d98c3a6516
CHAN-HO <chanho2547@gmail.com>
2022-11-02 17:49:55 +0900

    찬호/22.11.02

--- src/main/java/mini/dao/BoardDAO.java
+++ src/main/java/mini/dao/BoardDAO.java
@@ -1,5 +1,6 @@
 package mini.dao;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -18,12 +19,11 @@ public class BoardDAO {
 	private ResultSet rs = null;
 	private PreparedStatement pstmt = null;
 	
+/*
+ *   List<MemberVO>
+ */
 	
-	
-	
-	
-	
-	public List<MemberVO> memberSelect() {
+	public List<MemberVO> boardSelect() {
 		List<MemberVO> list = new ArrayList<>();
 		try {
 			conn = Common.getConnection();
@@ -32,6 +32,7 @@ public class BoardDAO {
 			rs = stmt.executeQuery(sql);
 			
 			while(rs.next()) {
+				
 				Integer docNum = rs.getInt("DOC_NUM");
 				Integer category = rs.getInt("CATEGORY");
 				String title = rs.getString("TITLE");
@@ -59,43 +60,102 @@ public class BoardDAO {
 		return list;
 	}
 	
-	
-	
-	
-	
-	
-	
-	// 게시글 가져오기 기능
-	// 게시글 고유번호로 찾기
-	// 정보가 많으니 배열로 반환?
-	// void로 만들어서 각자 set을 해준다 
-	public void findBoard (Integer boardNum) {
 		
+	
+/*
+	 게시글 가져오기 기능
+	 게시글 고유번호로 찾기
+	 정보가 많으니 List<MemberVO>반환?	 
+ */
+	
+	public List<MemberVO> findBoard (Integer boardNum) {
+		List<MemberVO> list = new ArrayList<>();
 		try {
 			conn = Common.getConnection();
 			stmt = conn.createStatement();
-			String sql = "SELECT * FROM BOARD WHERE DOC_NUM = "+ boardNum;
 			
+			String sql = "SELECT * FROM BOARD WHERE DOC_NUM = "+ boardNum;
 			rs = stmt.executeQuery(sql);
 			
 			while(rs.next()) {
+				Integer docNum = rs.getInt("DOC_NUM");
+				Integer category = rs.getInt("CATEGORY");
+				String title = rs.getString("TITLE");
+				String content = rs.getString("CONTENT");
+				String id = rs.getString("ID");
+				Date writeDate = rs.getDate("WRITE_DATE");
 				
+				MemberVO vo = new MemberVO();
+				
+				vo.setBoardNum(docNum);
+				vo.setCategory(category);
+				vo.setTitle(title);
+				vo.setBoardContent(content);
+				vo.setId(id);
+				vo.setBoardDate(writeDate);
+				list.add(vo);
 			}
 			
-			
-			
-			
+			// 닫기
+			Common.close(rs);
+			Common.close(stmt);
+			Common.close(conn);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return list;
 	}
 
 
 	
 	
+// 글쓰기 등록 기능 
+// 글쓰기 성공시 1리턴, 실패시 0리
+	
+public boolean boardRegister(Integer docNum, Integer category, String title, String content, String id) {
+	int result = 0;
+	String sql = "INSERT INTO BOARD(DOC_NUM, CATEGORY, TITLE, CONTENT, ID, WRITE_DATE) VALUES(?, ?, ?, ?, ?,SYSDATE)";
+	try {
+		conn = Common.getConnection();
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, docNum);
+		pstmt.setInt(2, category);
+		pstmt.setString(3, title);
+		pstmt.setString(4, content);
+		pstmt.setString(4, id);
+		result = pstmt.executeUpdate();	
+		System.out.println("글쓰기 DB 결과 확인 : " + result);
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	Common.close(rs);
+	Common.close(pstmt);
+	Common.close(conn);
+	
+	if(result == 1) return true;
+	else return false;
+}
+	
 
-	
-	
-	
+
+// 글 삭제 
+// 삭제 성공시 1, 실패시 0 반환
+public boolean boardDelete(Integer docNum) {
+	int result = 0;
+	String sql = "DELETE FROM BOARD WHERE ID = ? ";
+	try {
+		conn = Common.getConnection();
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, docNum);
+		result = pstmt.executeUpdate();
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+	Common.close(pstmt);
+	Common.close(conn);
+	if(result == 1) return true;
+	else return false;
+}
 }
---
+++ src/main/java/mini/servlet/board/boardListServlet.java
@@ -0,0 +1,91 @@
+package mini.servlet.board;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import mini.dao.MemberDAO;
+import mini.vo.MemberVO;
+
+import mini.common.Common;
+import mini.dao.BoardDAO;
+import mini.vo.*;
+
+/**
+ * Servlet implementation class boardListServlet
+ */
+@WebServlet("/boardListServlet")
+public class boardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Common.corsResSet(response);
+		request.setCharacterEncoding("utf-8");
+		
+		StringBuffer sb = Common.reqStringBuff(request);
+		JSONObject jsonObj = Common.getJsonObj(sb);
+		
+		String reqCmd = (String)jsonObj.get("cmd");
+		System.out.println("cmd : " + reqCmd);
+		PrintWriter out = response.getWriter();
+		
+		// 잘못 받아왔을 경우 
+		if(!reqCmd.equals("BoardInfo")) {
+			JSONObject resJson = new JSONObject();
+			resJson.put("result", "NOK");
+			out.print(resJson);
+			return;
+		}
+		
+		
+		BoardDAO dao = new BoardDAO();
+		List<MemberVO> list = dao.boardSelect();
+		
+		// 제이슨 형식 어레이 생성 
+		JSONArray boardArray = new JSONArray();
+		for (MemberVO e : list) {
+			// 자바 객체 생성
+			JSONObject boardInfo = new JSONObject();
+			boardInfo.put("boardNum", e.getBoardNum());
+			boardInfo.put("category", e.getCategory());
+			boardInfo.put("title", e.getTitle());
+			boardInfo.put("boardContent", e.getBoardContent());
+			boardInfo.put("id", e.getId());
+			DateFormat dateFormat = new SimpleDateFormat("YYYY/dd/MM HH:mm:ss");
+			String dateToStr = dateFormat.format(e.getBoardDate());
+			boardInfo.put("boardDate", dateToStr);
+			boardArray.add(boardInfo);
+		}
+		System.out.println(boardArray);
+		out.print(boardArray);
+
+	}
+
+	/**
+	 * @see HttpServlet#doOptions(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Common.corsResSet(response);
+	}
+
+}
